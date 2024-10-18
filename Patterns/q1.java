47bbdfac49c50ef21c042c6960571e9581d913a7
Debi Dutta Dash <131425103+Debidutta3@users.noreply.github.com>
2024-10-19 00:34:23 +0530

    Add files via upload

---
+++ Patterns/q1.java
@@ -0,0 +1,14 @@
+package JAVA.Patterns;
+
+public class q1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
